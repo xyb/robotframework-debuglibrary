3349d6909a72c5d52db27725fe76e9439c4e1c93
Xie Yanbo <xieyanbo@gmail.com>
2016-06-23 11:50:21 +0800

    bump to v0.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
 
 setup(
     name = 'robotframework-debuglibrary',
-    version = '0.4',
+    version = '0.5',
     description = 'RobotFramework debug library and an interactive shell',
     long_description = README,
     author = 'Xie Yanbo',
