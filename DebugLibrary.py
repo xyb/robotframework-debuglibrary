f1651b163f317b29ad5614cfd975e92baadaf48e
Xie Yanbo <xieyanbo@gmail.com>
2017-08-11 19:24:58 +0800

    release version 1.0.0

--- DebugLibrary.py
+++ DebugLibrary.py
@@ -66,7 +66,7 @@ from robot.libraries.BuiltIn import BuiltIn
 from robot.running.namespace import IMPORTER
 from robot.variables import is_var
 
-__version__ = '0.9.1'
+__version__ = '1.0.0'
 
 HISTORY_PATH = os.environ.get('RFDEBUG_HISTORY', '~/.rfdebug_history')
 KEYWORD_SEP = re.compile('  +|\t')
