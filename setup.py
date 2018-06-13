3f3cdb67cfd49e5ae6e83a3ee40cc5f88d79e42c
Xie Yanbo <xieyanbo@gmail.com>
2018-06-13 15:12:04 +0800

    Prompt_toolkit 2.0 is not compatible with 1.0, fixed #31

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ ROOT = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(ROOT, 'README.rst')).read()
 PY3 = sys.version_info[0] == 3
 
-install_requires = ['prompt-toolkit', 'pygments']
+install_requires = ['prompt-toolkit < 2', 'pygments']
 if PY3:
     install_requires.append('robotframework >= 3.0')
 else:
